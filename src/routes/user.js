949aaa1dc29f9a92cc17fef958eb738588018b16
GitGinocchio <giuliotognetto2@gmail.com>
2025-01-24 20:58:42 +0100

    project structure

---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+
+
+
+
+export default {
+    async fetch (request, env, ctx) {
+        const url = new URL(request.url);
+    }
+}
---
+++ src/worker.js
@@ -0,0 +1,16 @@
+
+import User from './routes/user.js';
+import Room from './routes/room.js';
+
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+
+        switch (url.pathname) {
+            case './routes/user.js':
+                User.fetch(request, env, ctx);
+            case './routes/room.js':
+                Room.fetch(request, env, ctx);
+        }
+    }
+}
