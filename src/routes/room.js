e0b4a09970f24007aaedf6b960d21c075ed239bd
GitGinocchio <giuliotognetto2@gmail.com>
2025-01-25 15:50:45 +0100

    handlebars

--- src/routes/room.js
+++ src/routes/room.js
@@ -1,9 +1,14 @@
 import Room from '../templates/room.html'
+import { compile } from 'handlebars';
 
 export default {
     async get (request, env, ctx) {
         const url = new URL(request.url);
 
-        return new Response(Room, { headers: { 'Content-Type': 'text/html' }});
+        const html = compile(Room)({
+
+        })
+
+        return new Response(html, { headers: { 'Content-Type': 'text/html' }});
     }
 }
