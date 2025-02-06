0bb1225074df0df3de5a9a59325d47e43fbffc05
ildesh <deshbertolesh@gmail.com>
2025-02-06 21:54:40 +0100

    Page 403 & 404
    - Implemented the 403 error page
    - Updated text and layout of the 404/403 pages for a clearer message
    - Reduced font size for the 404/403 error and increased the max-width of the image
    - Updated the 404 mascot image and added the 403 mascot

--- src/web/routes/403/route.js
+++ src/web/routes/403/route.js
@@ -1,8 +1,32 @@
+import Page403 from './403.html'
+import mustache from 'mustache';
 
-
+import Auth from '../../auth.js';
+import Utils from '../../utils.js';
 
 export default {
     async get (request, env, ctx) {
-        return new Response("Not Implemented");
+        const url = new URL(request.url);
+        const cookies = Utils.parseCookies(request.headers.get('cookie'));
+        
+        const token = cookies.get('user_access_token');
+        const payload = await Auth.verifyToken(token, env.JWT_SECRET_KEY);
+
+        let data = null;
+        if (payload != null){
+            const id = env.users.idFromString(payload.id);
+            const user = env.users.get(id);
+            data = await user.getUserData();
+        }
+
+        const html = mustache.render(Page403, { 
+            user : {
+                name : payload ? data.user.display_name : null, 
+                image : payload ? data.user.images[0].url : null, 
+                url: payload ? data.user.external_urls.spotify : null
+            },
+        });
+
+        return new Response(html, { headers: {'Content-Type': 'text/html'} });
     }
 }
