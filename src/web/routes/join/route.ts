a89954c1a8d2fd7d73551eaa5976c1cf43d88ec1
GitGinocchio <giuliotognetto2@gmail.com>
2025-02-23 10:34:44 +0100

    logs

--- src/web/routes/join/route.ts
+++ src/web/routes/join/route.ts
@@ -12,6 +12,8 @@ export default {
         const raw = await request.text();
         const params = Utils.parseParams(raw);
 
+        console.log("params: ", params);
+
         // @ts-ignore
         const room_token = await Auth.generateToken({ id : params.roomid }, env.ROOM_ACCESS_TOKEN_MAX_AGE, env.JWT_SECRET_KEY);
 
--- src/web/routes/room/route.ts
+++ src/web/routes/room/route.ts
@@ -67,7 +67,7 @@ export default {
         const user_data = await user.getData();
 
         const room_id = env.rooms.idFromString(room_payload.id);
-        console.log(room_id);
+        console.log("room_id: ", room_id);
         const room : Room = env.rooms.get(room_id);
         const room_data = await room.getData();
 
